a37d795f911ef17598896c358768826b22e57369
Eight-Corner <corner3499@gmail.com>
2021-04-01 22:35:07 +0900

    query sql

--- database/Scripts/userTable.sql
+++ database/Scripts/userTable.sql
@@ -4,17 +4,17 @@
 DROP TABLE TABLE_USER;
 
 CREATE TABLE TABLE_USER (
-  memberId varchar2(100) not null,
-  memberPw varchar2(100) not null,
-  memberName  varchar2(100),
-  memberEmail varchar2(200) not null,
-  memberEmailHash varchar2(200),
-  memberZipcode    varchar2(20) not null,
-  memberAddress    varchar2(300)not null,
-  memberAddressDETAIL varchar2(300),
-  memberAddressEtc  varchar2(100),
-  memberPoint NUMBER(10) NULL, -- 일단 NULL
-  CONSTRAINT EVENT_USER_PK PRIMARY KEY(memberId)
+  member_Id varchar2(100) not null,
+  member_Pw varchar2(100) not null,
+  member_Name  varchar2(100),
+  member_Email varchar2(200) not null,
+  member_Email_Hash varchar2(200),
+  member_Zipcode    varchar2(20) not null,
+  member_Address    varchar2(300)not null,
+  member_Address_DETAIL varchar2(300),
+  member_Address_Etc  varchar2(100),
+  member_Point NUMBER(10) NULL, -- 일단 NULL
+  CONSTRAINT EVENT_USER_PK PRIMARY KEY(member_Id)
 );
 
 -- ALTER TABLE TABLE_USER ADD memberPoint NUMBER(10) DEFAULT 300;
@@ -24,7 +24,7 @@ SELECT * FROM TABLE_USER;
 -------------
 
 INSERT INTO TABLE_USER
-(memberid, memberpw, membername, memberemail, memberemailhash, memberzipcode, memberaddress, memberaddressdetail, memberaddressetc)
+(member_Id, member_pw, member_name, member_email, member_email_hash, member_zipcode, member_address, member_address_detail, member_address_etc)
 VALUES('hds1234', '1234', '이벤트', 'hds1234@gmail.com', 'ddd', '16577', '경기도 수원시', '팔달구', '우만로 137');
 
 
