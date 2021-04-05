2f820146fc9d8670d619f5785fd602d722fdba3a
Eight-Corner <corner3499@gmail.com>
2021-04-05 22:12:54 +0900

    backUp

---
+++ workspace/eventMoa_0406/src/com/eventmoa/app/eventboard/vo/EventReplyVO.java
@@ -0,0 +1,47 @@
+package com.eventmoa.app.eventboard.vo;
+
+/*REPLY_NUM NUMBER(10),
+   BOARD_NUM NUMBER(10),
+   user_Id varchar2(100),
+   REPLY_CONTENT varchar2(4000),*/
+
+public class EventReplyVO {
+	private int reply_Num;
+	private int board_Num;
+	private String user_Id;
+	private String reply_Content;
+	
+	public EventReplyVO() {;}
+
+	public int getReply_Num() {
+		return reply_Num;
+	}
+
+	public void setReply_Num(int reply_Num) {
+		this.reply_Num = reply_Num;
+	}
+
+	public int getBoard_Num() {
+		return board_Num;
+	}
+
+	public void setBoard_Num(int board_Num) {
+		this.board_Num = board_Num;
+	}
+
+	public String getUser_Id() {
+		return user_Id;
+	}
+
+	public void setUser_Id(String user_Id) {
+		this.user_Id = user_Id;
+	}
+
+	public String getReply_Content() {
+		return reply_Content;
+	}
+
+	public void setReply_Content(String reply_Content) {
+		this.reply_Content = reply_Content;
+	}
+}
